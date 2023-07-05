4d30858c22d7d7a39e34c16782239f49f3475ee4
Dwiky24Kurniawan <dwiky24kurniawan@gmail.com>
2023-07-05 12:56:15 +0700

    update POM

--- POM/loginPage.js
+++ POM/loginPage.js
@@ -22,13 +22,6 @@ class LoginPage extends BasePage {
         return super.open('https://kasirdemo.belajarqa.com')
     }
 
-    async validLogin(email, password) {
-        await this.open()
-        await this.emailTextfield.setvalue(email)
-        await this.passwordTextfield.setvalue(password)
-        await this.loginButton.click()
-    }
-
 }
 
 export default new LoginPage();
